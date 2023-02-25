7f98d27398ab6a2cdb5e4d4d83119bd71a70b3a4
BattiestWinter <42357614+BattiestWinter@users.noreply.github.com>
2023-02-25 11:32:43 -0600

    Edición DKCntroller

--- Assets/Scripts/Gameplay/DKController.cs
+++ Assets/Scripts/Gameplay/DKController.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class DKController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public SpriteRenderer dkSprite;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+//Dependencia de la plataforma
+#if UNITY_ANDROID && !UNITY_EDITOR
+        if (Input.touchCount > 0)
+        {
+            fingerTouch = Input.GetTouch(0);
+            Debug.Log("Touch count: " + Input.touchCount);
+            Debug.Log("Touch position: " + fingerTouch.position);
+
+            if(fingerTouch.position.x < (Screen.width*0.5f))
+            {
+                dkSprite.flipX = true;
+            } 
+            else
+            {
+                dkSprite.flipX = false;
+            }
+            
+        }
+#endif
+
+#if UNITY_EDITOR
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            dkSprite.flipX = false;
+        }
+        else
+        {
+            dkSprite.flipX = true;
+        }
+#endif
     }
 }
